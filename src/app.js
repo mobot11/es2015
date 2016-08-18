25f7edf7ac5a384dd1a02b06ed85657b76102d4d
Dylan T. Moberg <dylanmoberg@msn.com>
2016-08-17 23:03:48 -0700

    extending classes, love that sugar

--- src/app.js
+++ src/app.js
@@ -3,3 +3,14 @@ import Dog from './moduleTest';
 const dog = new Dog('labrador', ' puppy');
 
 dog.print();
+
+class Cat extends Dog {
+
+  sound () {
+    console.log('meow');
+  }
+}
+
+const cat = new Cat('tabby', 'kitten');
+
+cat.sound();
